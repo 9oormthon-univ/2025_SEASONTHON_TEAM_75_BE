a5953fa79826a9405db730107266b4557b43b2e8
Junyeong-An <2117ab@gmail.com>
2025-09-01 04:34:36 +0900

    feat:#1 S3 에러코드 추가

--- src/main/java/com/trashheroesbe/global/response/type/ErrorCode.java
+++ src/main/java/com/trashheroesbe/global/response/type/ErrorCode.java
@@ -15,7 +15,10 @@ public enum ErrorCode {
     // auth
     NOT_EXISTS_TOKEN(HttpStatus.UNAUTHORIZED, "토큰이 존재하지 않습니다."),
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않는 토큰입니다."),
-    NOT_FOUND_ATTRIBUTES(HttpStatus.NOT_FOUND, "요청 attributes(uri)를 찾을 수 없습니다.")
+    NOT_FOUND_ATTRIBUTES(HttpStatus.NOT_FOUND, "요청 attributes(uri)를 찾을 수 없습니다."),
+
+    // s3
+    S3_UPLOAD_FAIL(HttpStatus.INTERNAL_SERVER_ERROR, "S3 파일 업로드에 실패했습니다."),
     ;
 
     private final HttpStatus status;
