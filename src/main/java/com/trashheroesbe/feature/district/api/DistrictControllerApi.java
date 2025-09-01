cd601830d2b8ddb1b7487a78d148f00e18700e3c
TaetaetaE01 <gogo589@naver.com>
2025-09-02 02:45:16 +0900

    feat: 자치구 조회 구현 #4

---
+++ src/main/java/com/trashheroesbe/feature/district/api/DistrictControllerApi.java
@@ -0,0 +1,21 @@
+package com.trashheroesbe.feature.district.api;
+
+import com.trashheroesbe.feature.district.dto.response.DistrictListResponse;
+import com.trashheroesbe.global.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
+
+@Tag(name = "District", description = "자치구 관련 API")
+public interface DistrictControllerApi {
+
+    @Operation(summary = "자치구 정보 조회하기", description = "시도, 시군구를 통해 자치구 정보를 조회한다.")
+    ApiResponse<List<DistrictListResponse>> getDistrictList(
+        @Parameter(description = "검색할 시도", example = "서울특별시")
+        String sido,
+
+        @Parameter(description = "검색할 시군구", example = "마포구")
+        String sigungu
+    );
+}
--- src/main/java/com/trashheroesbe/feature/user/application/UserService.java
+++ src/main/java/com/trashheroesbe/feature/user/application/UserService.java
@@ -53,5 +53,7 @@ public class UserService {
             user.updateProfileImageUrl(imageUrl);
         }
 
+
+
     }
 }
