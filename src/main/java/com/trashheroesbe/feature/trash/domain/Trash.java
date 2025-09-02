d7559ad9720b11065eb73b737c295e1118ada7af
Junyeong-An <dev.jun0321@gmail.com>
2025-09-02 15:37:04 +0900

    chore: #2 Trash 엔티티 수정

--- src/main/java/com/trashheroesbe/feature/trash/domain/Trash.java
+++ src/main/java/com/trashheroesbe/feature/trash/domain/Trash.java
@@ -2,15 +2,7 @@ package com.trashheroesbe.feature.trash.domain;
 
 import com.trashheroesbe.feature.user.domain.User;
 import com.trashheroesbe.global.entity.BaseTimeEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -42,11 +34,20 @@ public class Trash extends BaseTimeEntity {
     @Column
     private String summary;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "trash_type_id")
+    private TrashType trashType;
+
     public static Trash create(User user, String imageUrl, String name) {
         return Trash.builder()
                 .imageUrl(imageUrl)
                 .name(name)
                 .user(user)
                 .build();
+    }
+
+    public void applyAnalysis(TrashType type, String summary) {
+        this.trashType = type;
+        this.summary = summary;
     }
 }
