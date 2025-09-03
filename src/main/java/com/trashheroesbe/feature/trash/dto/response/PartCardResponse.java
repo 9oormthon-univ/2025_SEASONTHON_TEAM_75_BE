499d5bbe8d22d8f5b717154f4fd6cb2b4a21d13b
Junyeong-An <dev.jun0321@gmail.com>
2025-09-04 01:28:03 +0900

    feat: #15부품카드 추가

---
+++ src/main/java/com/trashheroesbe/feature/trash/dto/response/PartCardResponse.java
@@ -0,0 +1,13 @@
+package com.trashheroesbe.feature.trash.dto.response;
+
+import com.trashheroesbe.feature.trash.domain.Type;
+
+public record PartCardResponse(
+        String name,
+        String typeCode,
+        String typeName
+) {
+    public static PartCardResponse of(String name, Type type) {
+        return new PartCardResponse(name, type.getTypeCode(), type.getNameKo());
+    }
+}
