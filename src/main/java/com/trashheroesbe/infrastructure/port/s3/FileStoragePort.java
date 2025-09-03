28104eb1ae5a029ebc01795277d33edd19864e60
TaetaetaE01 <gogo589@naver.com>
2025-09-03 23:58:20 +0900

    refactor: s3 upload시 prefix 생성 수정

--- src/main/java/com/trashheroesbe/infrastructure/port/s3/FileStoragePort.java
+++ src/main/java/com/trashheroesbe/infrastructure/port/s3/FileStoragePort.java
@@ -1,6 +1,8 @@
 package com.trashheroesbe.infrastructure.port.s3;
 
 public interface FileStoragePort {
-    String uploadFile(String fileName, String pathPrefix, String contentType, byte[] fileData);
+
+    String uploadFile(String key, String contentType, byte[] fileData);
+
     void deleteFileByUrl(String fileUrl);
 }
