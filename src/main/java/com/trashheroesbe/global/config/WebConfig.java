950f11af0e4f47f519bb17678ae24e7405920032
TaetaetaE01 <gogo589@naver.com>
2025-09-03 23:34:40 +0900

    fix: HttpMessageConverter 추가하여 application/octet-stream 타입 오류 해결

--- src/main/java/com/trashheroesbe/global/config/WebConfig.java
+++ src/main/java/com/trashheroesbe/global/config/WebConfig.java
@@ -1,12 +1,19 @@
 package com.trashheroesbe.global.config;
 
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final MultipartJackson2HttpMessageConverter multipartConverter;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -25,5 +32,10 @@ public class WebConfig implements WebMvcConfigurer {
             .allowedOrigins("*")
             .allowedMethods("GET")
             .allowedHeaders("*");
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(multipartConverter);
     }
 }
