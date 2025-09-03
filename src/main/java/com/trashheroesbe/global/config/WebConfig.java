0187fbbe2414e823ee8882b084ec67ac28be5f6b
TaetaetaE01 <gogo589@naver.com>
2025-09-04 00:24:40 +0900

    refactor: cors 설정 수정

--- src/main/java/com/trashheroesbe/global/config/WebConfig.java
+++ src/main/java/com/trashheroesbe/global/config/WebConfig.java
@@ -18,8 +18,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-            .allowedOrigins("http://localhost:8080", "http://127.0.0.1:8080")
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedOriginPatterns(
+                "http://localhost:*",
+                "http://127.0.0.1:*",
+                "https://trash-heroes.store",
+                "https://*.trash-heroes.store"
+            )
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true);
 
