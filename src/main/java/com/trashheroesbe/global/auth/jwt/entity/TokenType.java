0f47afa4cefd8d8c4bfc19cb37ead4a25f2dde64
TaetaetaE01 <gogo589@naver.com>
2025-09-05 00:33:44 +0900

    refactor: 엑세스 토큰 만료 3시간으로 변경

--- src/main/java/com/trashheroesbe/global/auth/jwt/entity/TokenType.java
+++ src/main/java/com/trashheroesbe/global/auth/jwt/entity/TokenType.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum TokenType {
-    ACCESS_TOKEN("access_token", 1_800_000L), // 30분
+    ACCESS_TOKEN("access_token", 10_800_000L), // 3시간
     REFRESH_TOKEN("refresh_token", 1_209_600_000L); // 2주
 
     private final String name;
