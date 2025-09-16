27bc62bd82bbbb85d6a66b3085887e2efd2f448c
Junyeong-An <dev.jun0321@gmail.com>
2025-09-17 02:24:58 +0900

    chore: 필요없는 어노테이션 제거

--- src/main/java/com/trashheroesbe/feature/trash/infrastructure/PartRepository.java
+++ src/main/java/com/trashheroesbe/feature/trash/infrastructure/PartRepository.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface PartRepository extends JpaRepository<Part, Long> {
     Optional<Part> findByTrashTypeAndName(TrashType trashType, String name);
 }
