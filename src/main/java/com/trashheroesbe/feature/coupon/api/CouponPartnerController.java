0378f854eebbe55932292292bd9b3f2da15b06f6
Junyeong-An <dev.jun0321@gmail.com>
2025-12-15 23:58:42 +1000

    refactor: partner 경로 명시

--- src/main/java/com/trashheroesbe/feature/coupon/api/CouponPartnerController.java
+++ src/main/java/com/trashheroesbe/feature/coupon/api/CouponPartnerController.java
@@ -7,6 +7,7 @@ import com.trashheroesbe.feature.coupon.dto.request.CouponCreateRequest;
 import com.trashheroesbe.feature.coupon.dto.response.CouponCreateResponse;
 import com.trashheroesbe.feature.coupon.dto.response.CouponQrResponse;
 import com.trashheroesbe.global.response.ApiResponse;
+import com.trashheroesbe.global.auth.security.CustomerDetails;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,20 +16,22 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/partner")
 public class CouponPartnerController implements CouponPartnerControllerApi {
 
     private final CouponService couponService;
 
     @Override
-    @PostMapping("/partners/coupons")
+    @PostMapping("/coupons")
     public ApiResponse<CouponCreateResponse> createCoupon(
+            @AuthenticationPrincipal CustomerDetails customerDetails,
             @Valid @RequestBody CouponCreateRequest request
     ) {
-        return ApiResponse.success(OK, couponService.createCoupon(request));
+        return ApiResponse.success(OK, couponService.createCoupon(customerDetails, request));
     }
 
     @Override
