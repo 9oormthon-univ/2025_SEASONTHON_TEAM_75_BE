e6d975dba58a849de930e04dee9e1d889be9641e
TaetaetaE01 <gogo589@naver.com>
2025-12-30 22:35:39 +0900

    feat: 내 포인트 조회 구현

--- src/main/java/com/trashheroesbe/feature/point/application/PointService.java
+++ src/main/java/com/trashheroesbe/feature/point/application/PointService.java
@@ -1,17 +1,24 @@
 package com.trashheroesbe.feature.point.application;
 
+import com.trashheroesbe.feature.point.domain.entity.UserPoint;
 import com.trashheroesbe.feature.point.domain.event.PointEarnedEvent;
 import com.trashheroesbe.feature.point.domain.service.PointCalculator;
 import com.trashheroesbe.feature.point.domain.type.PointReason;
 import com.trashheroesbe.feature.point.dto.response.PointEarnedResult;
+import com.trashheroesbe.feature.point.dto.response.UserPointResponse;
+import com.trashheroesbe.feature.point.infrastructure.UserPointRepository;
+import com.trashheroesbe.feature.user.domain.entity.User;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class PointService {
+
+    private final UserPointRepository userPointRepository;
 
     private final ApplicationEventPublisher publisher;
     private final PointCalculator pointCalculator;
@@ -30,5 +37,16 @@ public class PointService {
 
         publisher.publishEvent(event);
         return new PointEarnedResult(earnedPoints, now);
+    }
+
+    @Transactional(readOnly = true)
+    public UserPointResponse getMyPoint(User user) {
+        UserPoint userPoint = userPointRepository.findByUserIdWithLock(user.getId())
+            .orElseGet(() -> {
+                UserPoint newUserPoint = UserPoint.createInit(user);
+                return userPointRepository.save(newUserPoint);
+            });
+
+        return UserPointResponse.from(userPoint);
     }
 }
