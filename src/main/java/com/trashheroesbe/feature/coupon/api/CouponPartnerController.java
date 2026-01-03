ab09f31bf1d863e2faf0781f5880d8eb57dbe67f
Junyeong-An <dev.jun0321@gmail.com>
2026-01-04 00:36:52 +0900

    feat: 파트너가 발급한 쿠폰 전체 조회기능 추가

--- src/main/java/com/trashheroesbe/feature/coupon/api/CouponPartnerController.java
+++ src/main/java/com/trashheroesbe/feature/coupon/api/CouponPartnerController.java
@@ -6,6 +6,7 @@ import com.trashheroesbe.feature.coupon.application.CouponService;
 import com.trashheroesbe.feature.coupon.dto.request.CouponCreateRequest;
 import com.trashheroesbe.feature.coupon.dto.request.CouponUpdateRequest;
 import com.trashheroesbe.feature.coupon.dto.response.CouponCreateResponse;
+import com.trashheroesbe.feature.coupon.dto.response.PartnerCouponResponse;
 import com.trashheroesbe.global.response.ApiResponse;
 import com.trashheroesbe.global.auth.security.CustomerDetails;
 import jakarta.validation.Valid;
@@ -14,10 +15,12 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,6 +28,14 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 public class CouponPartnerController implements CouponPartnerControllerApi {
 
     private final CouponService couponService;
+
+    @Override
+    @GetMapping("/coupons")
+    public ApiResponse<List<PartnerCouponResponse>> getPartnerCoupons(
+        @AuthenticationPrincipal CustomerDetails customerDetails
+    ) {
+        return ApiResponse.success(OK, couponService.getPartnerCoupons(customerDetails));
+    }
 
     @Override
     @PostMapping("/coupons")
